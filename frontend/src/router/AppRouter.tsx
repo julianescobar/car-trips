397017e2c001469faf9417f699e6079f9fa69ba3
Julián Escobar <julianandres.escobar03@gmail.com>
2025-11-20 13:00:12 -0500

    refactor: handle auth loading and update AppRouter

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,6 +1,5 @@
-// AppRouter.tsx
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../auth/context/AuthContext";
 import LoginPage from "../auth/pages/LoginPage";
 import { CarsPage } from "../cars/pages/CarsPage";
@@ -8,29 +7,20 @@ import { TripsPage } from "../trips/pages/TripsPage";
 import { LayoutLogged } from "../ui/LayoutLogged";
 import { SearchTripsPage } from "../trips/pages/SearchTripPage";
 
-
-const PrivateRoute = ({ children }: { children: JSX.Element }) => {
-  const { isAuthenticated } = useContext(AuthContext);
+const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, loadingAuth } = useContext(AuthContext);
+  if (loadingAuth) return null; 
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
-
-const PublicRoute = ({ children }: { children: JSX.Element }) => {
-  const { isAuthenticated } = useContext(AuthContext);
-  return isAuthenticated ? <Navigate to="/cars" /> : children;
+const PublicRoute = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, loadingAuth } = useContext(AuthContext);
+  if (loadingAuth) return null;
+  return isAuthenticated ? <Navigate to="/carros" /> : children;
 };
 
+
 export const AppRouter = () => {
-  const { logout } = useContext(AuthContext);
-
-  useEffect(() => {
-    const access = localStorage.getItem("access");
-    const refresh = localStorage.getItem("refresh");
-
-    if (!access || !refresh) {
-      logout();
-    }
-  }, [logout]);
 
   return (
     <Routes>
@@ -44,9 +34,10 @@ export const AppRouter = () => {
           </PublicRoute>
         }
       />
+
       
       <Route element={<LayoutLogged />}>
-      
+
         <Route
           path="/carros"
           element={
@@ -65,7 +56,7 @@ export const AppRouter = () => {
           }
         />
 
-         <Route
+        <Route
           path="/buscar-carro"
           element={
             <PrivateRoute>
@@ -73,6 +64,7 @@ export const AppRouter = () => {
             </PrivateRoute>
           }
         />
+
         
         <Route path="*" element={<Navigate to="/carros" />} />
 
